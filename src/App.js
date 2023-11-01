b57f7e037ff9e73c8b604a2a668ab19587a7cd15
priyanshu <buley2607@gmail.com>
2023-11-01 23:07:11 +0530

    Creating component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world</h1>
+        <Greeting/>
       </header>
     </div>
   );
