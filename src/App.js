07c6d2bdc70c08f472b1e302e0fb31d1ea51336f
priyanshu <buley2607@gmail.com>
2023-11-07 14:27:33 +0530

    esLint Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/Greeting';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Greeting/>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
--- src/components/Greeting.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Greeting() {
-  return (
-    <h1>Hello world</h1>
-  )
-}
-
-export default Greeting
