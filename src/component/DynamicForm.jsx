4763f27c5122d6b6bb960ad3690f713f297b8539
priyanshu buley <buley2607@gmail.com>
2023-11-22 23:36:16 +0530

    intial commit

--- src/component/DynamicForm.jsx
+++ src/component/DynamicForm.jsx
@@ -1,52 +1,54 @@
 import React, { useState } from 'react';
+import productList from '../constants/ProductList';
 
 function DynamicForm() {
-  const [input, setInput] = useState({ username: '', password: '' });
-  const [record, setRecord] = useState([]);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInput({ ...input, [name]: value });
+  const [index, setIndex] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  const totalPageNumber = productList.length;
+  const handleIncrement = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % totalPageNumber);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setRecord([...record, input]);
+
+  const handleDecrement = () => {
+    setIndex(
+      (prevIndex) => (prevIndex - 1 + totalPageNumber) % totalPageNumber
+    );
   };
-  const handleDelete = (para) => {
-    const removedUser = record.filter((user, i) => i !== para);
-    setRecord(removedUser);
+  const handleCart = () => {
+    setCartItems([...cartItems, productList[index].name]);
+    console.log(cartItems);
+  };
+  const handleRemove = (id) => {
+    const removedItems = cartItems.filter((items, i) => i !== id);
+    setCartItems(removedItems);
   };
   return (
-    <div>
-      <h1>Registration Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Name: </label>
-        <input
-          type="text"
-          name="username"
-          value={input.username}
-          onChange={handleChange}
-        />
-        <label htmlFor="password">Passsword: </label>
-        <input
-          type="password"
-          name="password"
-          value={input.password}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        <h4>Registered Users: </h4>
-        {record.map((user, i) => (
-          <li key={record.indexOf(user)}>
-            <h3>Name:{user.username}</h3>
-            <button type="button" onClick={() => handleDelete(i)}>
-              Delete
+    <>
+      <div className="index-button">
+        <button onClick={handleDecrement} type="button">
+          Previous Item
+        </button>
+        <button onClick={handleIncrement} type="button">
+          Next Item
+        </button>
+      </div>
+      <div className="items">
+        <h3>{productList[index].name}</h3>
+        <button type="button" onClick={handleCart}>
+          add to cart
+        </button>
+      </div>
+      <div className="sorted">
+        {cartItems.map((item, i) => (
+          <li key={item.id}>
+            <h5>{item}</h5>
+            <button type="button" onClick={() => handleRemove(i)}>
+              Remove
             </button>
           </li>
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/constants/ProductList.js
@@ -0,0 +1,64 @@
+const ProductList = [
+  { id: 1, name: 'iPhone 13', price: 799.99, category: 'Electronics' },
+  { id: 2, name: 'Nike Hoodie', price: 59.99, category: 'Clothing' },
+  { id: 3, name: 'Instant Pot', price: 89.99, category: 'Home & Kitchen' },
+  { id: 4, name: 'Samsung QLED TV', price: 1499.99, category: 'Electronics' },
+  { id: 5, name: 'MAC Lipstick', price: 19.99, category: 'Beauty' },
+  { id: 6, name: 'Levis Jeans', price: 49.99, category: 'Clothing' },
+  {
+    id: 7,
+    name: 'Dyson Vacuum Cleaner',
+    price: 399.99,
+    category: 'Home & Kitchen',
+  },
+  {
+    id: 8,
+    name: 'Sony Noise-Canceling Headphones',
+    price: 299.99,
+    category: 'Electronics',
+  },
+  { id: 9, name: 'Fenty Beauty Foundation', price: 34.99, category: 'Beauty' },
+  { id: 10, name: 'Google Nest Hub', price: 99.99, category: 'Electronics' },
+  {
+    id: 11,
+    name: 'KitchenAid Stand Mixer',
+    price: 249.99,
+    category: 'Home & Kitchen',
+  },
+  {
+    id: 12,
+    name: 'Fitbit Versa Smartwatch',
+    price: 179.99,
+    category: 'Electronics',
+  },
+  { id: 13, name: 'LOreal Shampoo', price: 9.99, category: 'Beauty' },
+  { id: 14, name: 'Calvin Klein T-shirt', price: 29.99, category: 'Clothing' },
+  { id: 15, name: 'Amazon Echo Dot', price: 49.99, category: 'Electronics' },
+  {
+    id: 16,
+    name: 'Cuisinart Coffee Maker',
+    price: 79.99,
+    category: 'Home & Kitchen',
+  },
+  {
+    id: 17,
+    name: 'Revlon Eyeshadow Palette',
+    price: 14.99,
+    category: 'Beauty',
+  },
+  { id: 18, name: 'Adidas Sneakers', price: 69.99, category: 'Clothing' },
+  {
+    id: 19,
+    name: 'Roku Streaming Stick',
+    price: 49.99,
+    category: 'Electronics',
+  },
+  {
+    id: 20,
+    name: 'Pyrex Glass Storage Set',
+    price: 39.99,
+    category: 'Home & Kitchen',
+  },
+];
+
+export default ProductList;
