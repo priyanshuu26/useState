ddf903f46e82fae2fc7ccaa8b587577df3518153
priyanshu buley <buley2607@gmail.com>
2023-11-23 10:59:15 +0530

    addding component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import UpdateDocumentTitle from './component/UpdateDocumentTitle';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <UpdateDocumentTitle />
+    </div>
+  );
 }
 
 export default App;
